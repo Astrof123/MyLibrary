69fd8dc38aff2292b48069a82fcf8c188fad0c39
Astrof123 <valeev_101@bk.ru>
2024-04-16 10:38:33 +0800

    Create all tests and files

---
+++ CircleShift/main.py
@@ -0,0 +1,17 @@
+def circle
+
+word = input()
+orig = input()
+total = 0
+temple = orig
+sp = []
+for i in range(len(orig)):
+    if temple not in sp:
+        for j in range(len(word) - len(orig) + 1):
+            if word[j:j+len(orig)] == temple:
+                total += 1
+        sp.append(temple)
+        temple = temple[-1] + temple[:-1]
+    else:
+        temple = temple[-1] + temple[:-1]
+print(total)
